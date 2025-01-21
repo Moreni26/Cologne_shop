b7347b404dc766f9ce36abc2bc82029949188347
moreni26 <ayomide2605@gmail.com>
2025-01-21 14:32:52 +0100

    recent updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,57 @@
 import React, { useContext, useEffect, useState } from "react";
 import { SidebarContext } from "../contexts/SidebarContext";
 import { CartContext } from "../contexts/CartContext";
-import { BsBag } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
-import Logo from '/assets/images/logo.png'
+import { BsBag } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
 const Header = () => {
-    const [isActive, setIsActive] = useState(false);
-    const {isOpen, setIsOpen} = useContext(SidebarContext);
-    const { itemAmount } = useContext(CartContext)
-    useEffect(() => {
-        window.addEventListener('scroll',() => {
-            window.scrollY > 60 ? setIsActive(true) : setIsActive(false);
-        });
+  const [isActive, setIsActive] = useState(false);
+  const { isOpen, setIsOpen } = useContext(SidebarContext);
+  const { itemAmount } = useContext(CartContext);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 60 ? setIsActive(true) : setIsActive(false);
     });
-    return <header className={`${isActive? 'bg-white py-4 shadow-md' : 'bg-none py-6'} fixed w-full z-10 transition-all` }>
-        <div className="container mx-auto flex items-center justify-between h-full">
-        <Link to={'/'}>
-        <div>
-            <img className="w-[50px] lg:w-[60px]" src={Logo} alt="" />
-        </div>
+  }, []);
+
+  return (
+    <header
+      className={`${
+        isActive ? "bg-white py-4 shadow-md" : "bg-none py-6"
+      } fixed w-full z-10 transition-all`}
+    >
+      <div className="container mx-auto flex items-center justify-between h-full">
+        {/* Brand Name */}
+        <Link to={"/"}>
+          <div className="text-3xl lg:text-4xl font-extrabold italic tracking-wide text-gray-900 mr-6">
+            <span className="text-red-500">THE</span> COLOGNE{" "}
+            <span className="text-red-500">SHOP</span>
+          </div>
         </Link>
-        <div onClick={()=> setIsOpen(!isOpen)} className="cursor-pointer flex relative">
-            <BsBag className="text-2xl"/>
-            <div className="bg-red-500 absolute -right-2 -bottom-2 text-[12px] w-[18px] h-[18px] text-white rounded-full flex justify-center items-center">{itemAmount}</div>
-            </div>
+
+        {/* Search bar */}
+        <div className="flex-1 px-8">
+          <input
+            type="text"
+            placeholder="Search for fragrances, brand and categories"
+            className="w-full py-2 px-4 bg-transparent border border-gray-300 rounded-lg shadow-sm placeholder-gray-400 text-gray-900 focus:outline-none focus:ring-2 focus:ring-black focus:border-black text-xl"
+          />          
         </div>
+
+        {/* Cart Icon */}
+        <div
+          onClick={() => setIsOpen(!isOpen)}
+          className="cursor-pointer flex relative"
+        >
+          <BsBag className="text-2xl" />
+          <div className="bg-red-500 absolute -right-2 -bottom-2 text-[12px] w-[18px] h-[18px] text-white rounded-full flex justify-center items-center">
+            {itemAmount}
+          </div>
+        </div>
+      </div>
     </header>
+  );
 };
 
 export default Header;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,39 +1,36 @@
 import React from "react";
 import Flower from "/assets/images/flower.jpg";
-
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Hero = () => {
-    return (
-      <section className="h-[880px] bg-no-repeat bg-cover bg-center py-24 items-center bg-yellow-50">
-        <div className="container mx-auto flex justify-around">
-          {/* text */}
-          <div className="flex flex-col justify-center">
-            <div className="font-semibold flex items-center uppercase">
-              <div className="w-10 h-[2px] bg-red-500 mr-3"></div>Cologne shop
-            </div>
-            <h1 className="text-[70px] leading-[1.1] font-light mb-4">
-              Discover the perfect fragrance that suits your style and
-              personality <br />
-              <span className="font-semibold">Fragrance</span>
-            </h1>
-            <Link
-              to={"/"}
-              className="font-semibold self-start uppercase border-b-2 border-black"
-            >
-              Explore More
-            </Link>
+  return (
+    <section
+      className="h-[880px] bg-no-repeat bg-cover bg-center py-24 items-center"
+      style={{
+        backgroundImage: `url(${Flower})`,
+      }}
+    >
+      <div className="container mx-auto flex justify-around">
+        {/* text */}
+        <div className="flex flex-col justify-center">
+          <div className="font-semibold flex items-center uppercase">
+            <div className="w-10 h-[2px] bg-red-500 mr-3"></div>Cologne shop
           </div>
-          <div className="hidden lg:block">
-            {/* <img
-              src={Flower}
-              alt=""
-              className="w-[600px] h-auto"
-            />*/}            
-          </div>
+          <h1 className="text-[70px] leading-[1.1] font-light mb-4">
+            Discover the perfect fragrance that suits your style and personality.{" "}
+            <br />
+            <span className="font-semibold">Fragrance</span>
+          </h1>
+          <Link
+            to={"/"}
+            className="font-semibold self-start uppercase border-b-2 border-black"
+          >
+            Explore More
+          </Link>
         </div>
-      </section>
-    );
+      </div>
+    </section>
+  );
 };
 
-export default Hero
+export default Hero;
